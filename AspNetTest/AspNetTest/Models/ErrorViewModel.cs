e49b877f1352a760725dabc8ade12160c193cefd
mrleo1nid <43583385+mrleo1nid@users.noreply.github.com>
2020-01-22 03:53:54 +0300

    Start Proj

---
+++ AspNetTest/AspNetTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspNetTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
