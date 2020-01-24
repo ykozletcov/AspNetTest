0e729f3662f62f3f9b0721a1b15d71a07b2abada
ykozletcov <mozg79@yandex.ru>
2020-01-25 00:50:16 +0400

    Update SqlRepository.cs
    Update and Delete added

--- AspNetTest/AspNetTest/Repository/SqlRepository.cs
+++ AspNetTest/AspNetTest/Repository/SqlRepository.cs
@@ -9,6 +9,11 @@ using Microsoft.Extensions.Logging;
 
 namespace AspNetTest.Repository
 {
+    public enum UpdMode
+    { 
+        Name,
+        Pwd
+    }
     public class SqlRepository
     {
         private readonly ILogger _logger;
@@ -47,6 +52,54 @@ namespace AspNetTest.Repository
             catch (Exception e)
             {
                 _logger.Log(LogLevel.Error, e.Message);
+                throw;
+            }
+        }
+
+
+        public int UpdateUser(int ID, UpdMode mode, string paramValue)
+        {
+            try
+            {
+                using (ApplicationContext db = new ApplicationContext())
+                {
+                    var user = db.Users.Where(x => x.ID == ID).First();
+                    if (mode == UpdMode.Name)
+                        user.Name = paramValue;
+                    else
+                        user.Password = paramValue;
+                    return db.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                _logger.Log(LogLevel.Error, e.ToString());
+                throw;
+            }
+        }
+
+
+        public int DeleteUser(int ID)
+        {
+            User user = GetUsers().Where(x => x.ID == ID).First();
+            return DeleteUser(user);
+        }
+
+        public int DeleteUser(User user)
+        {
+            if (user == null)
+                return -1;
+            try
+            {
+                using (ApplicationContext db = new ApplicationContext())
+                {
+                    db.Users.Remove(user);
+                    return db.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                _logger.Log(LogLevel.Error, e.ToString());
                 throw;
             }
         }
